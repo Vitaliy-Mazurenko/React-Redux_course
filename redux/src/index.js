bfb91e1b0b75ed35de6c821b8b063eb28b62e672
Vitaliy <vit007m@gmail.com>
2022-05-21 20:06:08 +0300

    reducer

---
+++ redux/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app'
+
+
+ReactDOM.render(
+	<>
+	<App/>
+	</>,
+	document.getElementById('root'));
